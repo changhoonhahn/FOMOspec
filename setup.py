3a3587da917718ac54b41d50589279566ef86fdc
ChangHoon Hahn <chh327@nyu.edu>
2018-07-02 14:45:21 -0400

    new repo for projects involved in forwardmodeling spectra

---
+++ setup.py
@@ -0,0 +1,20 @@
+#from setuptools import setup, find_packages
+from distutils.core import setup
+import numpy.distutils.misc_util
+import os, sys
+
+__version__ = '0.1'
+
+setup(name = 'FOMOspec',
+      version = __version__,
+      description = 'TBD',
+      author='ChangHoon Hahn',
+      author_email='changhoonhahn@lbl.gov',
+      url='',
+      platforms=['*nix'],
+      license='GPL',
+      requires = ['numpy', 'matplotlib', 'scipy'],
+      provides = ['FOMOspec'],
+      packages = ['FOMOspec'],
+      scripts=['fomospec/util.py']
+      )
