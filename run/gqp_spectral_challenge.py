58be6fcc11133b6eef278f2530ce1170d2e9f809
changhoonhahn <changhoonhahn@lbl.gov>
2019-01-09 17:37:51 -0800

    running firefly implemented for spectral analysis challenge

--- run/gqp_spectral_challenge.py
+++ run/gqp_spectral_challenge.py
@@ -10,12 +10,15 @@ import numpy as np
 import scipy as sp 
 from astropy.io import fits 
 from astropy import units as u
+import astropy.cosmology as co
 # -- desi --
 from desispec.io import read_spectra
 # -- feasibgs -- 
 from feasibgs import forwardmodel as FM 
 # -- fomospec -- 
 from fomospec import util as UT 
+from fomospec import spectra as Spec
+from fomospec import fitters as Fitters
 # -- plotting --
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -237,6 +240,57 @@ def lgal_bgsSpec(galid, iobs, lib='bc03', obs_sampling='spacefill', overwrite=Fa
     return bgs_spectra 
 
 
+def firefly_lgal_bgsSpec(galid, iobs, lib='bc03', obs_sampling='spacefill', 
+        model='m11', model_lib='MILES', imf='cha', hpf_mode='on'): 
+    ''' run firefly on simulated DESI BGS spectra from testGalIDs LGal
+    '''
+    obscond = obs_condition(sampling=obs_sampling) 
+    if iobs >= obscond.shape[0]: raise ValueError
+
+    # read in source spectra
+    if lib == 'bc03': 
+        f_name = 'gal_spectrum_'+str(galid)+'_BGS_template_BC03_Stelib.fits'
+    elif lib == 'fsps': 
+        f_name = 'gal_spectrum_'+str(galid)+'_BGS_template_FSPS_uvmiles.fits'
+    f_inspec = fits.open(''.join([UT.dat_dir(), 'Lgal/templates/', f_name]))
+    redshift = f_inspec[0].header['REDSHIFT']
+    
+    # read in simulated DESI bgs spectra
+    f_bgsspec = ''.join([UT.dat_dir(), 'spectral_challenge/bgs/', 
+        'BGSsim.gal_spectrum_', str(galid), '_BGS_template_BC03_Stelib.',
+        'obscond_', obs_sampling, '.', str(iobs+1), 'of', str(obscond.shape[0]), '.fits']) 
+    if not os.path.isfile(f_bgsspec): raise ValueError('spectra file does not exist')
+    spec_desi = read_spectra(f_bgsspec)
+    
+    gspec = Spec.GSfirefly()
+    gspec.DESIlike(spec_desi, redshift=redshift)
+    gspec.path_to_spectrum = UT.dat_dir()
+
+    # output firefly file 
+    f_firefly = ''.join([UT.dat_dir(), 'spectral_challenge/bgs/', 
+        'firefly.', model, '.', model_lib, '.imf_', imf, '.dust_', hpf_mode, '__', 
+        'BGSsim.gal_spectrum_', str(galid), '_BGS_template_BC03_Stelib.',
+        'obscond_', obs_sampling, '.', str(iobs+1), 'of', str(obscond.shape[0]), '.hdf5']) 
+
+    firefly = Fitters.Firefly(gspec,
+            f_firefly, # output file 
+            co.Planck13, # comsology
+            models = model, # model ('m11', 'bc03', 'm09') 
+            model_libs = [model_lib], # model library for M11
+            imfs = [imf], # IMF used ('ss', 'kr', 'cha')
+            hpf_mode = hpf_mode, # uses HPF to dereden the spectrum                       
+            age_limits = [0, 15], 
+            Z_limits = [-3., 5.], 
+            wave_limits = [3350., 9000.], 
+            suffix=None, 
+            downgrade_models = False, 
+            data_wave_medium = 'vacuum', 
+            use_downgraded_models = False, 
+            write_results = True)
+    bestfit = firefly.fit_models_to_data()
+    return None 
+
+
 # --- some plots --- 
 def plot_obs_condition(): 
     obscond = obs_condition(sampling='spacefill') 
@@ -343,10 +397,11 @@ def plot_lgal_bgsSpec():
 if __name__=="__main__": 
     #obs_condition(sampling='spacefill', overwrite=True)
     #obs_SkyBrightness(sampling='spacefill', overwrite=True)
-    #galids = testGalIDs()
-    #for iobs in range(1,8): 
-    #    for galid in galids: 
-    #        lgal_bgsSpec(galid, iobs, lib='bc03', obs_sampling='spacefill')
-    plot_obs_condition() 
-    plot_obs_SkyBrightness()
+    galids = testGalIDs()
+    for iobs in [0]: #range(1,8): 
+        for galid in galids[:1]: 
+            #lgal_bgsSpec(galid, iobs, lib='bc03', obs_sampling='spacefill')
+            firefly_lgal_bgsSpec(galid, iobs, lib='bc03', obs_sampling='spacefill', hpf_mode='on')
+    #plot_obs_condition() 
+    #plot_obs_SkyBrightness()
     #plot_lgal_bgsSpec()
