cf45953db92a658f270cc3915b5bc48067de4528
changhoonhahn <changhoonhahn@lbl.gov>
2019-11-06 07:24:26 -0800

    pip installable now

--- setup.py
+++ setup.py
@@ -1,20 +1,15 @@
-#from setuptools import setup, find_packages
-from distutils.core import setup
-import numpy.distutils.misc_util
-import os, sys
+#!/usr/bin/env python
+from setuptools import setup
 
 __version__ = '0.1'
 
 setup(name = 'FOMOspec',
       version = __version__,
-      description = 'TBD',
+      python_requires='>3.5.2',
+      description = 'Forward Modeling Observed Spectra',
       author='ChangHoon Hahn',
       author_email='changhoonhahn@lbl.gov',
-      url='',
-      platforms=['*nix'],
-      license='GPL',
       requires = ['numpy', 'matplotlib', 'scipy'],
       provides = ['fomospec'],
-      packages = ['fomospec'],
-      scripts=['fomospec/util.py']
+      packages = ['fomospec']
       )
